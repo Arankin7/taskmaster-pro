ee3fab960192c96f7e72fd68f75f127d0af2e8cc
AnthonyR <arankin789@gmail.com>
2022-03-30 12:11:30 -0400

    Drag and drop works! on mobile too

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,8 @@ $(".list-group").on("blur", "input[type='text']", function(){
   .val()
   .trim();
 
+  
+
   // get the parent ul's id attribute
   var status = $(this)
   .closest(".list-group")
@@ -134,6 +136,67 @@ $(".list-group").on("blur", "input[type='text']", function(){
   $(this).replaceWith(taskSpan);
 })
 
+// makes the list-group cards draggable and sortable across the colums
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event){
+  },
+  deactivate: function(event){
+  },
+  over: function(event){
+  },
+  out: function(event){
+  },
+  update: function(event){
+    // array to store the task data in 
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function(){
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui){
+    console.log("DROP");
+    ui.draggable.remove();
+  },
+  over: function(event, ui){
+    console.log("OVER");
+  },
+  out: function(event, ui){
+    console.log("OUT");
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
